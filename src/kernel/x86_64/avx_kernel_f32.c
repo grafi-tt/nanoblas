61e525d2f6cb81d050b4961909ac9b41e0022ef7
Shunsuke Shimizu <grafi@grafi.jp>
2017-06-25 02:09:25 +0900

    new kernel

--- src/kernel/x86_64/avx_kernel_f32.c
+++ src/kernel/x86_64/avx_kernel_f32.c
@@ -1,6 +1,11 @@
 #include "kernel/x86_64/avx_kernel.h"
 #include "internal/macro.h"
 
+void nanoblas_f32_avx_kernel_fun(kernel_state_t *kernel_st, prepack_state_t *prepack_st) {
+	/* to support both of Windows and SysV calling conventions */
+	nanoblas_f32_avx_kernel_asm(kernel_st, prepack_st, prepack_st, kernel_st);
+}
+
 kernel_t nanoblas_f32_avx_kernel = {
 	.fun = &nanoblas_f32_avx_kernel_fun,
 	.m_slice_len = 12,
--- src/kernel/x86_64/avx_kernel_f32.s
+++ src/kernel/x86_64/avx_kernel_f32.s
@@ -1,261 +1,420 @@
-/* ebp: sched_k_len               (escape to stack; loaded from stack)      */
-/* rsp: stack pointer                                                       */
-/* rdi: a_pack                                                              */
-/* rsi: b_pack                                                              */
-/* edx: k_len -> sched_m_len -> k_len                                       */
-/* ecx: m_sub_len -> sched_m_cnt                                            */
-/* r8:  n_sub_len -> a_next                                                 */
-/* r9:  c -> a_next_pack -> c                                               */
-/* r10: ldc -> proceed_k -> ldc                                             */
-/* r11: interval_m                                                          */
-/* r12: k_len                     (escape to stack)                         */
-/* rax: tmp                                                                 */
-/* rbx: tmp                       (escape to stack)                         */
-/* r13: reserved                                                            */
-/* r14: reserved                                                            */
-/* r15: reserved                                                            */
+// offsets in kernel_staet_t struct
+.set offset_a_pack_cur,        0
+.set offset_b_pack_cur,        8
+.set offset_c_cur,             16
+.set offset_ldc,               24
+.set offset_m_slice_real_len,  32
+.set offset_n_slice_real_len,  36
+.set offset_k_len,             40
+
+// offsets in prepack_state_t
+.set offset_next_cur,          0
+.set offset_next_pack_cur,     8
+.set offset_sched_len,         16
+.set offset_mn_slice_real_len, 20
+.set offset_mn_slice_len,      24
+.set offset_interval_mn,       32
+.set offset_proceed_k,         40
 
 .section .text
+.balign 32
+loadmask:
+// [1.0, ..., 1.0]
+.rept 8
+.float 1
+.endr
+// [0, ..., 0, -1.0, ..., -1.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -1
+.endr
+// [0, ..., 0, -2.0, ..., -2.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -2
+.endr
+// [0, ..., 0, -3.0, ..., -3.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -3
+.endr
+// [0, ..., 0, -4.0, ..., -4.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -4
+.endr
+// [0, ..., 0, -5.0, ..., -5.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -5
+.endr
+// [0, ..., 0, -6.0, ..., -6.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -6
+.endr
+// [0, ..., 0, -7.0, ..., -7.0]
+.rept 8
+.float 0
+.endr
+.rept 8
+.float -7
+.endr
 
-.align 4
-.globl nanoblas_f32_avx_kernel_fun
-nanoblas_f32_avx_kernel_fun:
-	/* escape */
-	pushq %rbp
-	pushq %rbx
-	pushq %r12
-	/* create mask template */
-	movl $0x10000000, %eax
-	shll $23, %ecx
-	addl %ecx, %eax
-	pushq %rax
-	vbroadcastss (%rsp), %xmm0
-	movaps %xmm0, %xmm1
-	/* shift template */
-	leaq shifter(%rip), %rax
-	leaq (%r8,%r8,4), %r8
-	addq %r8, %rax
-	jmpq *%rax
-	/* 40 bytes until here */
-	/* loop */
-shifter:
-	psrldq $4, %xmm1
-	psrldq $4, %xmm1
-	psrldq $4, %xmm1
-	psrldq $4, %xmm1
-	psrldq $4, %xmm0
-	psrldq $4, %xmm0
-	psrldq $4, %xmm0
-	psrldq $4, %xmm0
-	/* 80 bytes until here */
-	/* cocnat template */
-	vinsertf128 $1, %xmm1, %ymm0, %ymm0
-	/* backup template */
-	vmovaps %ymm0, %ymm5
+.globl nanoblas_f32_avx_pack_asm
+nanoblas_f32_avx_pack_asm:
+	movq offset_next_cur(%rdx), %r8
+	movq offset_interval_mn(%rdx), %rcx
+	cmpl $4, %ecx
+	jne pack_trans
 
-	/* mask updater for n-dim */
-	/* IEEE754 trick */
-	movq $0x00800000, (%rsp) /* 2^23 */
-	vbroadcastss (%rsp), %ymm1
+	// ymm0 is reserved by nanoblas_avx_kernel_asm
+	// ymm1 is anyway [1.0, ..., 1.0]
+	// ymm2 will be the template of load mask
+	// see comment in nanoblas_f32_avx_kernel_asm for detail, at the place creating mask to load C
+	movl offset_sched_len(%rdx), %eax
+	shll $4, %eax
+	addl offset_mn_slice_real_len(%rdx), %eax
+	leaq loadmask(%rip), %r9
+	vmovups -32(%r9, %rax, 4), %ymm2
+	vmovaps (%r9), %ymm1
 
-	/* escape c */
-	pushq %r9
-	/* ldc */
-	movq 40(%rsp), %r10
-	/* load c  */
-	vmaskmovps (%r9), %ymm0, %ymm8
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm9
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm10
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm11
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm12
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm13
-	vaddps %ymm1, %ymm0, %ymm0
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm14
-	vaddps %ymm1, %ymm0, %ymm0
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps (%r9), %ymm0, %ymm15
+	// pack data
+	// ymm3 is the temporary
+	movq offset_next_pack_cur(%rdx), %r9
 
-	/* load sched state */
-	mov 48(%rsp),%r8
-	test %r8, %r8
-	jz avx_kernel_nopack_loop
-	/* adjust and escape k_len */
-	mov 40(%r8), %ebp
-	leal (,%ebp,8), %eax
-	subl %eax, %edx
-	movl %edx, %r12d
-	/* load vals */
-	mov 44(%r8), %edx
-	mov 32(%r8), %r11
-	mov 24(%r8), %r10
-	mov 8(%r8), %r9
-	mov (%r8), %r8
-	/* calculate proceed_k */
-	leaq (,%r11,8), %rax
-	subq %rax, %r10
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, (%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 32(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 64(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 96(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 128(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 160(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 192(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vmovaps %ymm3, 224(%r9)
+	vaddps %ymm1, %ymm2, %ymm2
 
-.align 4
-avx_kernel_pack_loop:
-	/* ymm1 = (a'[8:4], a'[8:4]); ymm0 = (a'[4:0], a'[4:0]) */
-	vmovaps (%rdi), %ymm0
-	vperm2f128 $0x11, %ymm0, %ymm0, %ymm1
-	vperm2f128 $0x00, %ymm0, %ymm0, %ymm0
-	/* ymm4 = b' */
-	vmovaps (%rsi), %ymm4
-	/* c[0,] += a'[0] * b'; c[1,] += a[1] * b' */
-	vshufps $0x00, %ymm0, %ymm0, %ymm2
-	vshufps $0x55, %ymm0, %ymm0, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm8, %ymm6, %ymm8
-	vaddps %ymm9, %ymm7, %ymm9
-	/* c[2,] += a'[2] * b'; c[3,] += a'[3] * b' */
-	vshufps $0xAA, %ymm0, %ymm0, %ymm2
-	vshufps $0xFF, %ymm0, %ymm0, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm10, %ymm6, %ymm10
-	vaddps %ymm11, %ymm7, %ymm11
-	/* c[4,] += a'[4] * b'; c[5,] += a'[5] * b' */
-	vshufps $0x00, %ymm1, %ymm1, %ymm2
-	vshufps $0x55, %ymm1, %ymm1, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm12, %ymm6, %ymm12
-	vaddps %ymm13, %ymm7, %ymm13
-	/* c[6,] += a'[6] * b'; c[7,] += a'[7] * b' */
-	vshufps $0xAA, %ymm1, %ymm1, %ymm2
-	vshufps $0xFF, %ymm1, %ymm1, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm14, %ymm6, %ymm14
-	vaddps %ymm15, %ymm7, %ymm15
-	/* increment a_pack and b_pack */
-	leaq 32(%rdi), %rdi
-	leaq 32(%rsi), %rsi
+	ret
 
-	/* scheduled pack */
-	xorl %eax, %eax
-	cmpl %edx, %ecx
-	jge after_load
-		movl (%r8), %eax
-	after_load:
-	movl %eax, (%r9)
-	leaq (%r8,%r11,4), %r8
-	addq $4, %r9
-	add $1, %edx
-	xorl %eax, %eax
-	cmp $8, %edx
-	sete %al
-	addq $-1, %rax
-	andl %eax, %ecx
-	notq %rax
-	movq %r10, %rbx
-	andq %rax, %rbx
-	leaq (%r8,%rbx,4), %r8
-	addl %eax, %ebp
-	jnz avx_kernel_pack_loop
-	/* restore k_len */
-	mov %r12d, %edx
+.balign 16
+pack_trans:
+	movq offset_interval_k(%rdx), %rcx
+	movl offset_mn_slice_real_len(%rdx), %eax
 
-.align 4
+	// ymm0 is reserved by nanoblas_avx_kernel_asm
+	// ymm1 is anyway [1.0, ..., 1.0]
+	// ymm2 will be the template of load mask
+	// see comment in nanoblas_f32_avx_kernel_asm for detail, at the place creating mask to load C
+	movl offset_mn_slice_real_len(%rdx), %eax
+	shll $4, %eax
+	addl offset_sched_len(%rdx), %eax
+	leaq loadmask(%rip), %r9
+	vmovups -32(%r9, %rax, 4), %ymm2
+	vmovaps (%r9), %ymm1
+
+	// transpose algorithm is from
+	// Optimization Manual 11.11.2
+
+	// copy data to buf, and do first step of transpose
+	// ymm3 is the temporary
+	// ymm4-ymm7 are working
+	// note that ymm8-ymm15 are reserved by nanoblas_avx_kernel_asm
+	movq %rsp, %rcx
+	andq $-32, %rsp
+	subq $256, %rsp
+
+	vmaskmovps (%r8), %ymm2, %ymm4
+	vmovaps %ymm4, (%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm5
+	vmovaps %ymm5, 32(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm6
+	vmovaps %ymm6, 64(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm7
+	vmovaps %ymm7, 96(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vinsertf128 $1, %xmm3, %ymm4, %ymm4
+	vmovaps %ymm3, 128(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vinsertf128 $1, %xmm3, %ymm5, %ymm5
+	vmovaps %ymm3, 160(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vinsertf128 $1, %xmm3, %ymm6, %ymm6
+	vmovaps %ymm3, 192(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+	addq %rcx, %r8
+	vmaskmovps (%r8), %ymm2, %ymm3
+	vinsertf128 $1, %xmm3, %ymm7, %ymm7
+	vmovaps %ymm3, 224(%rsp)
+	vaddps %ymm1, %ymm2, %ymm2
+
+	// second step
+	movq offset_next_pack_cur(%rdx), %r9
+
+	vunpcklpd %ymm5, %ymm4, %ymm2
+	vunpckhpd %ymm5, %ymm4, %ymm3
+	vunpcklpd %ymm7, %ymm6, %ymm4
+	vunpckhpd %ymm7, %ymm6, %ymm5
+
+	vshufps $0x88, %ymm4, %ymm2, %ymm6
+	vmovaps %ymm6, (%r9)
+	vshufps $0xDD, %ymm4, %ymm2, %ymm6
+	vmovaps %ymm6, 32(%r9)
+	vshufps $0x88, %ymm5, %ymm3, %ymm6
+	vmovaps %ymm6, 64(%r9)
+	vshufps $0xDD, %ymm5, %ymm3, %ymm6
+	vmovaps %ymm6, 96(%r9)
+
+	// third step
+	vmovaps 16(%rsp), %xmm4
+	vinsertf128 $1, 144(%rsp), %ymm4, %ymm4
+	vmovaps 48(%rsp), %xmm5
+	vinsertf128 $1, 176(%rsp), %ymm5, %ymm5
+	vunpcklpd %ymm5, %ymm4, %ymm2
+	vunpckhpd %ymm5, %ymm4, %ymm3
+
+	vmovaps 80(%rsp), %xmm6
+	vinsertf128 $1, 208(%rsp), %ymm6, %ymm6
+	vmovaps 112(%rsp), %xmm7
+	vinsertf128 $1, 240(%rsp), %ymm7, %ymm7
+	vunpcklpd %ymm7, %ymm6, %ymm4
+	vunpckhpd %ymm7, %ymm6, %ymm5
+
+	// fourth step
+	vshufps $0x88, %ymm4, %ymm2, %ymm6
+	vmovaps %ymm6, 128(%r9)
+	vshufps $0xDD, %ymm4, %ymm2, %ymm6
+	vmovaps %ymm6, 160(%r9)
+	vshufps $0x88, %ymm5, %ymm3, %ymm6
+	vmovaps %ymm6, 192(%r9)
+	vshufps $0xDD, %ymm5, %ymm3, %ymm6
+	vmovaps %ymm6, 224(%r9)
+
+	movq %rcx, %rsp
+	ret
+
+.balign 16
+.globl nanoblas_f32_avx_kernel_asm
+nanoblas_f32_avx_kernel_asm:
+	// create mask to load C
+	// ymm3 will be [0, ..., 0, -m_slice_real_len, ..., -m_slice_real_len]
+	//          #(8 - n_slice_real_len)     #n_slice_rean_len
+	movl offset_m_slice_real_len(%rcx), %eax
+	shll $4, %eax
+	addl offset_n_slice_real_len(%rcx), %eax
+	leaq loadmask(%rip), %r9
+	vmovups -32(%r9, %rax, 4), %ymm3
+	// save ymm3 to ymm0
+	vmovaps %ymm3, %ymm0
+	// ymm1 will be [1.0, ..., 1.0]
+	vmovaps (%r9), %ymm1
+
+	// load C
+	movq offset_c_cur(%rcx), %r10
+	movq offset_ldc(%rcx), %r11
+	// [ymm8^T, ..., ymm15^T]^T = C (8x8)
+	vmaskmovps (%r10), %ymm3, %ymm8
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm9
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm10
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm11
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm12
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm13
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm14
+	vaddps %ymm1, %ymm3, %ymm3
+	addq %r11, %r10
+	vmaskmovps (%r10), %ymm3, %ymm15
+
+	// loop_len_remained: eax
+	movl offset_k_len(%rcx), %eax
+	// prepare for duff's device
+	movl %eax, %r9d
+	negl %r9d
+	andl $7, %r9d
+	// loop length is 16*9
+	// jump address: r10
+	shll $4, %r9d
+	leaq (%r9, %r9, 8), %r8
+	leaq avx_kernel_nopack_loop(%rip), %r10
+	addq %r8, %r10
+	// multiply displacement by 32, and shift it by 128 for shorter instruction encoding
+	negq %r9
+	addq %r9, %r9
+	subq $-128, %r9
+	// a_pack_cur: r8
+	// b_pack_cur: r9
+	movq offset_a_pack_cur(%rcx), %r8
+	addq %r9, %r8
+	addq offset_b_pack_cur(%rcx), %r9
+
+	// push jump address
+	push %r10
+	// prepare for store, and leave rcx for packing
+	movq offset_c_cur(%rcx), %r10
+	// jump to loop
+	nopl (%rax)
+	jmp *(%rsp)
+	// 16 bytes alinged here
+
 avx_kernel_nopack_loop:
-	/* ymm1 = (a'[8:4], a'[8:4]); ymm0 = (a'[4:0], a'[4:0]) */
-	vmovaps (%rdi), %ymm0
-	vperm2f128 $0x11, %ymm0, %ymm0, %ymm1
-	vperm2f128 $0x00, %ymm0, %ymm0, %ymm0
-	/* ymm4 = b' */
-	vmovaps (%rsi), %ymm4
-	/* c[0,] += a'[0] * b'; c[1,] += a[1] * b' */
-	vshufps $0x00, %ymm0, %ymm0, %ymm2
-	vshufps $0x55, %ymm0, %ymm0, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm8, %ymm6, %ymm8
-	vaddps %ymm9, %ymm7, %ymm9
-	/* c[2,] += a'[2] * b'; c[3,] += a'[3] * b' */
-	vshufps $0xAA, %ymm0, %ymm0, %ymm2
-	vshufps $0xFF, %ymm0, %ymm0, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm10, %ymm6, %ymm10
-	vaddps %ymm11, %ymm7, %ymm11
-	/* c[4,] += a'[4] * b'; c[5,] += a'[5] * b' */
-	vshufps $0x00, %ymm1, %ymm1, %ymm2
-	vshufps $0x55, %ymm1, %ymm1, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm12, %ymm6, %ymm12
-	vaddps %ymm13, %ymm7, %ymm13
-	/* c[6,] += a'[6] * b'; c[7,] += a'[7] * b' */
-	vshufps $0xAA, %ymm1, %ymm1, %ymm2
-	vshufps $0xFF, %ymm1, %ymm1, %ymm3
-	vmulps %ymm2, %ymm4, %ymm6
-	vmulps %ymm3, %ymm4, %ymm7
-	vaddps %ymm14, %ymm6, %ymm14
-	vaddps %ymm15, %ymm7, %ymm15
-	/* increment a_pack and b_pack */
-	leaq 32(%rdi), %rdi
-	leaq 32(%rsi), %rsi
-	subl $8, %edx
-	jnz avx_kernel_nopack_loop
-avx_kernel_loop_end:
 
-	/* ldc, c */
-	movq 40(%rsp), %r10
-	popq %r9
-	/* load template */
-	vbroadcastss (%rsp), %ymm1
+.macro unroll cnt=0, times
+	.balign 16
+	// ymm3 = (a[8:4], a[8:4]); ymm2 = (a[4:0], a[4:0])
+	vmovaps 32*\cnt-128(%r8), %ymm2
+	vinsertf128 $0, 32*\cnt-128+16(%r8), %ymm2, %ymm3
+	vinsertf128 $1, 32*\cnt-128(%r8), %ymm2, %ymm2
+	// ymm5 = b
+	vmovaps 32*\cnt-128(%r9), %ymm5
+
+	// ymm4 = [a[0], ..., a[0]]
+	vshufps $0x00, %ymm2, %ymm2, %ymm4
+	// C[0,] += a[0] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm8, %ymm4, %ymm8
+	// ymm4 = [a[1], ..., a[1]]
+	vshufps $0x55, %ymm2, %ymm2, %ymm4
+	// C[1,] += a[1] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm9, %ymm4, %ymm9
+	// ymm4 = [a[2], ..., a[2]]
+	vshufps $0xAA, %ymm2, %ymm2, %ymm4
+	// C[2,] += a[2] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm10, %ymm4, %ymm10
+	// ymm4 = [a[3], ..., a[3]]
+	vshufps $0xFF, %ymm2, %ymm2, %ymm4
+	// C[3,] += a[3] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm11, %ymm4, %ymm11
+	// ymm4 = [a[4], ..., a[4]]
+	vshufps $0x00, %ymm3, %ymm3, %ymm4
+	// C[4,] += a[4] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm12, %ymm4, %ymm12
+	// ymm4 = [a[5], ..., a[5]]
+	vshufps $0x55, %ymm3, %ymm3, %ymm4
+	// C[5,] += a[5] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm13, %ymm4, %ymm13
+	// ymm4 = [a[6], ..., a[6]]
+	vshufps $0xAA, %ymm3, %ymm3, %ymm4
+	// C[6,] += a[6] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm14, %ymm4, %ymm14
+	// ymm4 = [a[7], ..., a[7]]
+	vshufps $0xFF, %ymm3, %ymm3, %ymm4
+	// C[7,] += a[7] * b
+	vmulps %ymm4, %ymm5, %ymm4
+	vaddps %ymm15, %ymm4, %ymm15
+
+	.if (\cnt+1)-\times
+	unroll (\cnt+1), \times
+	.else
+	addq $32*\times, %r8
+	addq $32*\times, %r9
+	subl $\times, %eax
+	.endif
+.endm
+
+	unroll times=8
+	jg avx_kernel_nopack_loop
 	addq $8, %rsp
-	/* store c */
-	vmaskmovps %ymm8, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm9, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm10, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm11, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm12, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm13, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	vaddps %ymm1, %ymm1, %ymm1
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm14, %ymm5, (%r9)
-	vaddps %ymm1, %ymm5, %ymm5
-	leaq (%r9,%r10,4), %r9
-	vmaskmovps %ymm15, %ymm5, (%r9)
+	// loop end
 
-	/* return */
-	popq %r12
-	popq %rbx
-	popq %rbp
+	// update cursor
+	addq $-128, %r8
+	addq $-128, %r9
+	movq %r8, offset_a_pack_cur(%rcx)
+	movq %r9, offset_b_pack_cur(%rcx)
+
+	// if required, do packing
+	test %rdx, %rdx
+	jnz store_c
+	call nanoblas_f32_avx_pack_asm
+
+.balign 16
+store_c:
+	// store c
+	// c is already on r10
+	// ldc is already on r11
+	vmaskmovps %ymm8, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm9, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm10, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm11, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm12, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm13, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm14, %ymm0, (%r10)
+	vaddps %ymm1, %ymm0, %ymm0
+	addq %r11, %r10
+	vmaskmovps %ymm15, %ymm0, (%r10)
+
 	ret
